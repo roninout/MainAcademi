85ec8142a203f1cb015a92486b9681badd20a14f
ron_in <roninorgua@gmail.com>
2019-03-17 14:14:20 +0200

    Добавил 2-й модуль с лабами

---
+++ MA/Basics/Module 2/LabWork2_1/LibraryUser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LabWork2_1
+{
+
+    // 1) declare interface ILibraryUser
+    // declare method's signature for methods of class LibraryUser
+
+
+
+    // 2) declare class LibraryUser, it implements ILibraryUser
+
+
+    // 3) declare properties: FirstName (read only), LastName (read only), 
+    // Id (read only), Phone (get and set), BookLimit (read only)
+
+    // 4) declare field (bookList) as a string array
+
+
+    // 5) declare indexer BookList for array bookList
+
+    // 6) declare constructors: default and parameter
+
+    // 7) declare methods: 
+
+    //AddBook() – add new book to array bookList
+
+    //RemoveBook() – remove book from array bookList
+
+    //BookInfo() – returns book info by index
+
+    //BooksCout() – returns current count of books
+
+}
