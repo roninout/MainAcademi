d1874e140739f9a012f1151488a3d060a361ff1d
ron_in <roninorgua@gmail.com>
2019-03-21 20:57:44 +0200

    final

--- MA/Basics/Module 1/Lesson_3/HomeWork_3/Rand/Program.cs
+++ MA/Basics/Module 1/Lesson_3/HomeWork_3/Rand/Program.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Rand
 {
@@ -15,43 +12,49 @@ namespace Rand
 
             Random random = new Random();
             Console.WriteLine("Угадайте число в диапазоне от 0 до 5!");
-
-            Console.WriteLine("Сделайте свой выбор: ");
-            int userNumber = Int32.Parse(Console.ReadLine());
-            while (true)
+            try
             {
-                // random.Next(MaxValue) returns a 32-bit signed integer that is greater than or equal to 0 and less than MaxValue
-                int Guess_number = random.Next(MyMax) + 1;
-                // implement input of number and comparison result message in the while circle with  comparison condition
-                Console.WriteLine($"Количество попыток: {count}");
-                Console.WriteLine($"My - {Guess_number}, your - {userNumber}. ");
-                if (Guess_number == userNumber)
+                Console.WriteLine("Сделайте свой выбор: ");
+                int userNumber = int.Parse(Console.ReadLine());
+                while (true)
                 {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("Very nice");
-                    Console.ResetColor();
-                    Console.WriteLine("Любая кнопка для выхода....");
-                    Console.ReadKey();
-                    break;
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine($"Maybe next time");
-                    Console.ResetColor();
-                }
+                    // random.Next(MaxValue) returns a 32-bit signed integer that is greater than or equal to 0 and less than MaxValue
+                    int Guess_number = random.Next(MyMax) + 1;
+                    // implement input of number and comparison result message in the while circle with  comparison condition
+                    Console.WriteLine($"Количество попыток: {count}");
+                    Console.WriteLine($"My - {Guess_number}, your - {userNumber}. ");
+                    if (Guess_number == userNumber)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Very nice");
+                        Console.ResetColor();
+                        Console.WriteLine("Любая кнопка для выхода....");
+                        Console.ReadKey();
+                        break;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"Maybe next time");
+                        Console.ResetColor();
+                    }
 
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("Может еще разок? Для выхода введи Exit");
-                Console.ResetColor();
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Может еще разок? Для выхода введи Exit");
+                    Console.ResetColor();
 
-                string choise = Console.ReadLine();
-                if (choise.ToUpper() == "EXIT")
-                    break;
-                else
+                    string choise = Console.ReadLine();
+                    if (choise.ToUpper() == "EXIT") break;
                     userNumber = Int32.Parse(choise);
-                count++;
-                Console.Clear();
+                    count++;
+                    Console.Clear();
+                }
+            }
+            catch (Exception)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Были введены некорректные данные. Программы будет завершена!");
+                Console.ReadKey();
             }
         }
     }
