c46308aa06e44c2d9a13b7fa065fc72638c805f9
ron_in <roninorgua@gmail.com>
2019-03-21 11:55:28 +0200

    tg

--- MA/Basics/Module 1/LabWork1_3_1/Program.cs
+++ MA/Basics/Module 1/LabWork1_3_1/Program.cs
@@ -14,9 +14,45 @@ namespace LabWork1_3_1
             int count = 1;
 
             Random random = new Random();
-            // random.Next(MaxValue) returns a 32-bit signed integer that is greater than or equal to 0 and less than MaxValue
-            int Guess_number = random.Next(MyMax) + 1;
-            // implement input of number and comparison result message in the while circle with  comparison condition
+            Console.WriteLine("Угадайте число в диапазоне от 0 до 5!");
+
+            Console.WriteLine("Сделайте свой выбор: ");
+            int userNumber = Int32.Parse(Console.ReadLine());
+            while (true)
+	        {
+                // random.Next(MaxValue) returns a 32-bit signed integer that is greater than or equal to 0 and less than MaxValue
+                int Guess_number = random.Next(MyMax) + 1;
+                // implement input of number and comparison result message in the while circle with  comparison condition
+                Console.WriteLine($"Количество попыток: {count}");
+                Console.WriteLine($"My - {Guess_number}, your - {userNumber}. ");
+                if (Guess_number == userNumber)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Very nice");
+                        Console.ResetColor();
+                        Console.WriteLine("Любая кнопка для выхода....");
+                        Console.ReadKey();
+                        break;
+                    }
+                else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"Maybe next time");
+                        Console.ResetColor();
+                    }
+                
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Может еще разок? Для выхода введи Exit");
+                Console.ResetColor();
+
+                string choise = Console.ReadLine();
+                if (choise.ToUpper() == "EXIT")
+                    break;
+                else
+                    userNumber = Int32.Parse(choise);
+                count++;
+                Console.Clear();
+	        }
         }
     }
 }
