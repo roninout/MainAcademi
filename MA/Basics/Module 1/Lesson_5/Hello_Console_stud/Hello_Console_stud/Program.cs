c7cb2e1328bdc247378707dfccc4a846cf17db3c
ron_in <roninorgua@gmail.com>
2019-03-29 19:54:37 +0200

    Last

--- MA/Basics/Module 1/Lesson_5/Hello_Console_stud/Hello_Console_stud/Program.cs
+++ MA/Basics/Module 1/Lesson_5/Hello_Console_stud/Hello_Console_stud/Program.cs
@@ -242,8 +242,8 @@ namespace Hello_Console_stud
                 morseChar = FindMorseSimbol(Dictionary_arr, sosCharItem.ToString());
                 if (morseChar != null)                // проверка на отсутсвие символа
                 {
-                    Console.Write($"{sosCharItem} "); // выводим символы на экран
-                    Console.WriteLine(morseChar);     // не понятно?????
+                    Console.WriteLine($"{sosCharItem} {new string(morseChar)}"); // выводим символы на экран
+                    //Console.WriteLine(morseChar);     // не понятно?????
                     PlayMorseChar(morseChar);         // проигрываем морзе символ
                 }
             }
