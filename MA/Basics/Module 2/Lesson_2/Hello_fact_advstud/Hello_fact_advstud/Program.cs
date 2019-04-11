59893d7ecddbbe840ce60e8daadaa9c5f502c680
ron_in <roninorgua@gmail.com>
2019-04-12 00:27:42 +0300

    fact - done

--- MA/Basics/Module 2/Lesson_2/Hello_fact_advstud/Hello_fact_advstud/Program.cs
+++ MA/Basics/Module 2/Lesson_2/Hello_fact_advstud/Hello_fact_advstud/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 
 namespace Hello_fact_advstud
 {
@@ -12,13 +13,20 @@ namespace Hello_fact_advstud
         {
             //Define parameters to calculate the factorial of
             //Call fact() method to calculate
+            Console.WriteLine(fact(20));
+            Console.ReadKey();
         }
 
         //Create fact() method  with parameter to calculate factorial
-        //Use ternary operator
+        static BigInteger fact(int n)
+        {
+            //Use ternary operator
+            return (n <= 1) ? 1 : n * fact(n - 1);
+        }
+        
 
     }
 
-    
+
 
 }
