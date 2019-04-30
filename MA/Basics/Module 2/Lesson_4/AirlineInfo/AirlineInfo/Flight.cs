e1ea99b5eea1bfc880b3824629b80569e529b9ed
ron_in <roninorgua@gmail.com>
2019-04-30 17:08:10 +0300

    next

--- MA/Basics/Module 2/Lesson_4/AirlineInfo/AirlineInfo/Flight.cs
+++ MA/Basics/Module 2/Lesson_4/AirlineInfo/AirlineInfo/Flight.cs
@@ -85,7 +85,48 @@ namespace AirlineInfo
         }
 
         // редактирование
-        public abstract void Edit(Columns columns, int selectType);
+        // редактирование ВЫЛЕТ-ов
+        public virtual void Edit(Columns columns, int selectType)
+        {
+            switch (columns)
+            {
+                case Columns.colFlightNumber:
+                    FlightNumber = Console.ReadLine();
+                    break;
+                case Columns.colDateTime:
+                    DateTime = DateTime.Parse(Console.ReadLine());
+                    break;
+                case Columns.colCityPort:
+                    CityPort = Console.ReadLine();
+                    break;
+                case Columns.colAirline:
+                    Airline = Console.ReadLine();
+                    break;
+                case Columns.colTerminal:
+                    Terminal = Console.ReadLine();
+                    break;
+                case Columns.colGate:
+                    Gate = Console.ReadLine();
+                    break;
+                case Columns.colFlightStatus:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine(@"Для изменения СТАТУСА выберите следующие варианты:
+    0 -  регистрация
+    1 - гейт закрыт
+    2 - прибыл
+    3 - отправляется в ...
+    4 - неизвестен
+    5 - отменен
+    6 - ожидается в ...
+    7 - задерживается
+    8 - в полёте");
+                    FlightStatus = (FlightStatus)int.Parse(Console.ReadLine());
+                    break;
+                default:
+                    break;
+            }
+            Console.Clear();
+        }
 
         #endregion
     }
