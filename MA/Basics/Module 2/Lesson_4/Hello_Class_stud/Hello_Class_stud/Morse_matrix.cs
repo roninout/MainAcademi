491532e75b31d6aa3356a5487443574d5b18d11d
ron_in <roninorgua@gmail.com>
2019-04-30 19:56:53 +0300

    Crypt/Decrypt

--- MA/Basics/Module 2/Lesson_4/Hello_Class_stud/Hello_Class_stud/Morse_matrix.cs
+++ MA/Basics/Module 2/Lesson_4/Hello_Class_stud/Hello_Class_stud/Morse_matrix.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Hello_Class_stud
 {
@@ -7,23 +8,26 @@ namespace Hello_Class_stud
     {
         public const int Size_2 = Alphabet.Size;
         private int offset_key = 0;
-        private string[,] Dict_arr;
 
         #region constructor
         //Implement Morse_matrix constructor with the int parameter for offset
         public Morse_matrix(int offset_key = 0)
         {
             this.offset_key = offset_key;
+            fd(Alphabet.Dictionary_arr);
+            sd();
         }
 
         //Implement Morse_matrix constructor with the string [,] Dict_arr and int parameter for offset
         public Morse_matrix(string[,] dict_arr, int offset_key = 0) : this(offset_key)
         {
-            Dict_arr = dict_arr;
+            fd(Alphabet.Dictionary_arr);
+            sd();
         }
         #endregion
 
         //Use fd(Alphabet.Dictionary_arr) and sd() methods
+        // заполняем матрицу
         private void fd(string[,] Dict_arr)
         {
             for (int ii = 0; ii < Size1; ii++)
@@ -32,6 +36,7 @@ namespace Hello_Class_stud
         }
 
         //Use fd(Dict_arr) and sd() methods
+        // смещаем одну колонку в матрице с ключем смещения
         private void sd()
         {
             int off = Size_2 - offset_key;
@@ -48,20 +53,56 @@ namespace Hello_Class_stud
         //Use indexers
         public string crypt(string word)
         {
-            throw new NotImplementedException();
+            string result = String.Empty;
+            for (int i = 0; i < word.Length; i++)
+            {
+                for (int j = 0; j < Size2; j++)
+                {
+                    if (Char.Parse(this[0, j]) == word[i])
+                        result += this[1, j].ToString();
+                }
+            }
+            return result;
         }
 
         //Realize decrypt() with string array parameter
         //Use indexers
-        public string decrypt(string[] sos)
+        public string decrypt(string[] signal)
         {
-            throw new NotImplementedException();
+            string result = string.Empty;
+            for (int i = 0; i < signal.Length; i++)
+            {
+                for (int j = 0; j < Size2; j++)
+                {
+                    if (signal[i] == this[1, j])
+                        result += this[0, j];
+                }
+            }
+            return result;
         }
 
         //Implement Res_beep() method with string parameter to beep the string
         public void Res_beep(string rslt)
         {
-
+            foreach (char morseCharItem in rslt.ToCharArray())
+            {
+                switch (morseCharItem)
+                {
+                    case '.':
+                        Console.Beep(1000, 250); //Implement Console.Beep(1000, 250) for '.'
+                        Thread.Sleep(50); //Use Thread.Sleep(50) to separate sounds
+                        break;
+                    case '-':
+                        Console.Beep(1000, 750); // and Console.Beep(1000, 750) for '-'
+                        Thread.Sleep(50); //Use Thread.Sleep(50) to separate sounds
+                        break;
+                    case ' ':
+                        Thread.Sleep(50); //Use Thread.Sleep(50) to separate sounds
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
 
     }
