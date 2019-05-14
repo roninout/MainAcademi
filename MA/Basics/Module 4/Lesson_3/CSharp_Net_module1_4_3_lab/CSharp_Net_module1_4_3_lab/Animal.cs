30784adc995cef01edbd36c5a73c0ae6ebbe9ca7
ron_in <roninorgua@gmail.com>
2019-05-15 00:10:20 +0300

    derer

--- MA/Basics/Module 4/Lesson_3/CSharp_Net_module1_4_3_lab/CSharp_Net_module1_4_3_lab/Animal.cs
+++ MA/Basics/Module 4/Lesson_3/CSharp_Net_module1_4_3_lab/CSharp_Net_module1_4_3_lab/Animal.cs
@@ -6,27 +6,44 @@ namespace CSharp_Net_module1_4_3_lab
     // 12) change methods of sorting to properties
 
     // 1) implement interface IComparable
-    public class Animal
+    public class Animal : IComparable
     {
         // 2) declare properties and parameter constructor
- 
+        public string Genus { get; set; }
+        public int Weight { get; set; }
+
+        public Animal(string genus, int weight)
+        {
+            Genus = genus;
+            Weight = weight;
+        }
+
         // 3) implement method ComareTo()
-        // it comares Genus of type string - return result of method String.Compare 
-        // for this.genus and argument object
-        // don't forget to cast object to Animal
- 
+        public int CompareTo(object obj)
+        {
+            // it comares Genus of type string - return result of method String.Compare 
+            // for this.genus and argument object
+            // don't forget to cast object to Animal
+            var animal = obj as Animal;
+            return animal != null ? Genus.CompareTo(animal.Genus) : throw new Exception("Невозможно сравнить два объекта");
+        }
+
 
         // 4) declare methods SortWeightAscending(), SortGenusDescending()
+        public static IComparer SortWeightAscending()
+        {
+            return
+        }
         // they are static and return IComparer
         // return type is custed from constructor of classes SortWeightAscendingHelper, 
         // SortGenusDescendingHelper calling 
-  
+
 
         // 5) declare 2 nested private classes SortWeightAscendingHelper, SortGenusDescendingHelper 
         // they implement interface IComparer
         // every nested class has implemented method Comare with 2 parameters of object and return int
         // class SortWeightAscendingHelper sort weight by ascending
         // class SortGenusDescendingHelper sort genus by descending
- 
-      }
+
+    }
 }
