acc72e3db39ba9d13788eda61d81337c77c82af9
ron_in <roninorgua@gmail.com>
2019-05-17 21:48:48 +0300

    weuydfieu

---
+++ MA/Basics/Module 4/Lesson_3/ClassWork/ClassWork_4/CustomCollection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork_4
+{
+    class CustomCollection : ICollection
+    {
+        private readonly Array array;
+
+        public CustomCollection(Type type, int lenght)
+        {
+            this.array = Array.CreateInstance(type, lenght);
+        }
+
+        public int Count => array.Length;
+
+        public object SyncRoot => this;
+
+        public bool IsSynchronized => (array as ICollection).IsSynchronized;
+
+        public void CopyTo(Array array, int index)
+        {
+            //Array.CopyTo(array,this.array;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                yield return array.GetValue(i);
+            }
+        }
+    }
+}
