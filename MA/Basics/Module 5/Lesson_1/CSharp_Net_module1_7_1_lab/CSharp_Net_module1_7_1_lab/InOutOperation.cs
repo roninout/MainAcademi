11bf7f5637ed9e4e18ce792d6204bf6bca37a2af
ron_in <roninorgua@gmail.com>
2019-05-30 16:47:32 +0300

    edfd

--- MA/Basics/Module 5/Lesson_1/CSharp_Net_module1_7_1_lab/CSharp_Net_module1_7_1_lab/InOutOperation.cs
+++ MA/Basics/Module 5/Lesson_1/CSharp_Net_module1_7_1_lab/CSharp_Net_module1_7_1_lab/InOutOperation.cs
@@ -9,41 +9,163 @@ using System.Threading;
 
 namespace CSharp_Net_module1_7_1_lab
 {
-    class InOutOperation
+    static class InOutOperation
     {
-        // 1) declare properties  CurrentPath - path to file (without name of file), CurrentDirectory - name of current directory,
-        // CurrentFile - name of current file
-      
+        #region properties
+        // 1) declare properties
+        public static string CurrentPath { get; set; }         // CurrentPath - path to file (without name of file)
+        public static string CurrentDirectory { get; set; }    // CurrentDirectory - name of current directory
+        public static string CurrentFile { get; set; }         // CurrentFile - name of current file
+        #endregion
 
+        #region methods
         // 2) declare public methods:
         //ChangeLocation() - change of CurrentPath; 
         // method takes new file path as parameter, creates new directories (if it is necessary)
-        
+        public static void ChangeLocation(string newFilePath)
+        {
+            var curDirectory = new DirectoryInfo(newFilePath);
+            if (!curDirectory.Exists)
+                curDirectory.Create();
+
+            CurrentPath = curDirectory.FullName;
+        }
+
         // CreateDirectory() - create new directory in current location
+        public static void CreateDirectory(string newDir)
+        {
+            var curDirectory = new DirectoryInfo(CurrentPath);
+
+            if (!curDirectory.Exists)
+                curDirectory.CreateSubdirectory(newDir);
+        }
 
         // WriteData() – save data to file
         // method takes data (info about computers) as parameter
+        public static void WriteData(List<Computer> computers)
+        {
+            var curDirectory = new DirectoryInfo(CurrentPath);
+            if (!curDirectory.Exists)
+                curDirectory.Create();
+
+            // Создаем файл.
+            var file = new FileInfo(CurrentPath + "\\" + CurrentFile);
+
+            // С помощью экземпляра StreamWriter записываем в файл несколько строк текста. 
+            using (StreamWriter writer = file.AppendText())
+            {
+                foreach (var computer in computers)
+                {
+                    writer.WriteLine($"{computer.Cores},{computer.Frequency},{computer.Hdd},{computer.Memory}");
+                }
+                Console.WriteLine($"Сохранение в файл {file} завершено. Исходный размер: {file.Length.ToString()}.");
+            }
+        }
 
         // ReadData() – read data from file
         // method returns info about computers after reading it from file
-        
+        public static void ReadData()
+        {
+            string input;
+            var file = new FileInfo(CurrentPath + "\\" + CurrentFile);
+            if (file.Exists)
+            {
+                using (StreamReader reader = file.OpenText())
+                {
+                    // Выводим содержимое файла в консоль.
+                    while ((input = reader.ReadLine()) != null)
+                    {
+                        string[] result = input.Split(',');
+                        Console.WriteLine($"Cores: {result[0]},Frequency: {result[1]},Hdd: {result[2]},Memory: {result[3]}");
+                    }
+                }
+            }                    
+        }
+
         // WriteZip() – save data to zip file
         // method takes data (info about computers) as parameter
-        
+        public static void WriteZip()
+        {
+            string sourceFile = CurrentPath + "\\" + CurrentFile;
+            string[] temp = CurrentFile.Split('.');
+            string compressedFile = $"{CurrentPath}\\{temp[0]}.gz";
+
+            // поток для чтения исходного файла
+            using (FileStream sourceStream = new FileStream(sourceFile, FileMode.OpenOrCreate))
+            {
+                // поток для записи сжатого файла
+                using (FileStream targetStream = File.Create(compressedFile))
+                {
+                    // поток архивации
+                    using (GZipStream compressionStream = new GZipStream(targetStream, CompressionMode.Compress))
+                    {
+                        sourceStream.CopyTo(compressionStream); // копируем байты из одного потока в другой
+                        Console.WriteLine($"Сжатие файла {sourceFile} завершено. Исходный размер: {sourceStream.Length.ToString()}  сжатый размер: {targetStream.Length.ToString()}.");
+                    }
+                }
+            }
+        }
+
         // ReadZip() – read data from file
         // method returns info about computers after reading it from file
+        public static void ReadZip()
+        {
+            string[] temp = CurrentFile.Split('.');
+            string compressedFile = $"{CurrentPath}\\{temp[0]}.gz";
+            string input;
+
+            // поток для чтения из сжатого файла
+            using (FileStream sourceStream = new FileStream(compressedFile, FileMode.OpenOrCreate))
+            {
+                // поток разархивации
+                using (GZipStream decompressionStream = new GZipStream(sourceStream, CompressionMode.Decompress))
+                {
+                    using (StreamReader sr = new StreamReader(decompressionStream))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            if ((input = sr.ReadLine()) != null)
+                            {
+                                string[] result = input.Split(',');
+                                Console.WriteLine($"Cores: {result[0]},Frequency: {result[1]},Hdd: {result[2]},Memory: {result[3]}");
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
         // ReadAsync() – read data from file asynchronously
         // method is async
         // method returns Task with info about computers
         // use ReadLineAsync() method to read data from file
         // Note: don't forget about await
+        public static async Task ReadAsync()
+        {
+            string sourceFile = CurrentPath + "\\" + CurrentFile;
+            string input;
+
+            using (StreamReader reader = new StreamReader(sourceFile))
+            {
+                while ((input = await reader.ReadLineAsync()) != null) // асинхронное чтение из файла
+                {
+                    string[] result = input.Split(',');
+                    Console.WriteLine($"Cores: {result[0]},Frequency: {result[1]},Hdd: {result[2]},Memory: {result[3]}");
+                }
+            }
+
+            //await Task.Run(() => ReadData());
+        }
 
         // 7)
         // ADVANCED:
         // WriteToMemoryStream() – save data to memory stream
         // method takes data (info about computers) as parameter
         // info about computers is saved to Memory Stream
+        public static void WriteToMemoryStream()
+        {
+
+        }
 
         // use  method GetBytes() from class UnicodeEncoding to save array of bytes from string data 
         // create new file stream
@@ -52,7 +174,11 @@ namespace CSharp_Net_module1_7_1_lab
 
         // WriteToFileFromMemoryStream() – save data to file from memory stream and read it
         // method takes file stream as parameter
-        // save data to file      
+        // save data to file
+        public static void WriteToFileFromMemoryStream()
+        {
+
+        }
 
 
         // Note: 
@@ -61,7 +187,7 @@ namespace CSharp_Net_module1_7_1_lab
         // - don't forget to check path before any file or directory operations
         // - don't forget to check existed directory and file before creating
         // use File class to check files, Directory class to check directories, Path to check path
-        
 
+        #endregion
     }
 }
--- MA/Basics/Module 5/Lesson_1/CSharp_Net_module1_7_1_lab/CSharp_Net_module1_7_1_lab/Program.cs
+++ MA/Basics/Module 5/Lesson_1/CSharp_Net_module1_7_1_lab/CSharp_Net_module1_7_1_lab/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,18 +14,51 @@ namespace CSharp_Net_module1_7_1_lab
         {
             // 3) create collection of computers;
             // set path to file and file name
+            List<Computer> computers = new List<Computer>();
+            computers.Add(new Computer() { Cores = 5, Frequency = 2500, Hdd = 10, Memory = 8});
+            computers.Add(new Computer() { Cores = 6, Frequency = 3500, Hdd = 20, Memory = 4 });
+            computers.Add(new Computer() { Cores = 7, Frequency = 4500, Hdd = 30, Memory = 6 });
+
+            InOutOperation.CurrentFile = "test.txt";
+            InOutOperation.CurrentPath = @"D:\InOutOperation";
+
+            // удаляем все перед запуском
+            var curDirectory = new DirectoryInfo(InOutOperation.CurrentPath);
+            if (curDirectory.Exists)
+                curDirectory.Delete(true);
 
             // 4) save data and read it in the seamplest way (with WriteData() and ReadData() methods)
+            Console.WriteLine("Обычная запись в файл");
+            InOutOperation.WriteData(computers);
+            Console.WriteLine();
+            Console.WriteLine("Чтение данных с файла");
+            InOutOperation.ReadData();
+            Console.WriteLine();
 
             // 5) save data and read it with WriteZip() and ReadZip() methods
             // Note: create another file for these operations
+            Console.WriteLine("Сжатая запись в файл");
+            InOutOperation.WriteZip();
+            Console.WriteLine();
+
+            Console.WriteLine("Чтение данных с жатого файла");
+            InOutOperation.ReadZip();
+            Console.WriteLine();
 
             // 6) read info about computers asynchronously (from the 1st file)
             // While asynchronous method will be running, Main() method must print ‘*’ 
-            
+
+            Console.WriteLine("Чтение данных с файла асинхронно");
+            Task readAsync = InOutOperation.ReadAsync();
+            Console.WriteLine();
+
+
             // use 
             // collection of Task with info about computers as type to get returned data from method ReadAsync()
             // use property Result of collection of Task to get access to info about computers
+            List<Task> list = new List<Task>();
+            list.Add(InOutOperation.ReadAsync());
+            list.Add(InOutOperation.ReadAsync());
 
             // Note:
             // print all info about computers after reading from files
@@ -38,8 +72,8 @@ namespace CSharp_Net_module1_7_1_lab
 
             // call method WriteToFileFromMemory() with parameter of file stream
             // open file with saved data and compare it with input info
-            
-                        
+
+            Console.WriteLine("Done!");
             Console.ReadKey();
         }
     }
