ffce30cdcd6ab8f576e8f4451d6427b7b77de613
ron_in <roninorgua@gmail.com>
2019-07-17 21:42:05 +0300

    ененен

---
+++ MA/Professional/Module 2/Lesson_2/ClassWork/ClassWork_1/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace ClassWork_1
+{
+    public class Class1
+    {
+        public static void OnlyThreadMeth()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"Counter: {i}, thread: {Thread.CurrentThread.ManagedThreadId}");
+                Thread.Sleep(10);
+            }
+        }
+
+        public static void FirstThread()
+        {
+            ThreadStart starter = new ThreadStart(OnlyThreadMeth);
+            Thread thr_1 = new Thread(starter);
+            Thread thr_2 = new Thread(starter);
+
+            thr_1.Start();
+            thr_2.Start();
+
+            thr_1.Join();
+            thr_2.Join();
+
+            Console.Read();
+        }
+    }
+}
---
+++ MA/Professional/Module 2/Lesson_2/ClassWork/ClassWork_2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClassWork_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Intrrpt();
+        }
+
+        #region Interrupt
+        public static Thread mother;
+        public static Thread son;
+        public static void Intrrpt()
+        {
+            mother = new Thread(new ThreadStart(MotherSleep));
+            son = new Thread(new ThreadStart(ChildWoke));
+            mother.Start();
+            son.Start();
+            mother.Join();
+            son.Join();
+            Console.WriteLine("Exiting Interrupt");
+            Console.ReadLine();
+        }
+
+        private static void MotherSleep()
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                Console.Write(" m ");
+                if ((i % 10 == 0) & (i != 0))
+                {
+                    try
+                    {
+                        Console.WriteLine("Mother counter control: {0}, thread: {1}. ", i, Thread.CurrentThread.ManagedThreadId + " Mother will sleep");
+                        Thread.Sleep(20);
+                    }
+                    catch (ThreadInterruptedException e)
+                    {
+                        Console.WriteLine("Mother catch ThreadInterruptedException. Get up! ");
+                    }
+                    Console.WriteLine("Ok");
+                }
+            }
+        }
+        private static void ChildWoke()
+        {
+            for (int i = 1; i < 50; i++)
+            {
+                Console.Write(" c ");
+                if (mother.ThreadState == ThreadState.WaitSleepJoin)
+                {
+                    Console.WriteLine("Child check Mother.ThreadState == ThreadState.WaitSleepJoin. Interrupting mother");
+                    mother.Interrupt();
+                }
+            }
+
+        }
+        #endregion
+    }
+}
